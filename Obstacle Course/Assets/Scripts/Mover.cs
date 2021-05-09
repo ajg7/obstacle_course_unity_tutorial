70e64615d4e07bcac83f27743f1766304a3f377e
AJ <ajgebara@gmail.com>
2021-05-08 22:26:39 -0500

    methods

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
-    public float moveSpeed = 10f;
+    
+    [SerializeField] public float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer() 
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, 0, zValue);
     }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Hello World");
+        Debug.Log("Move your Player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls");
+    }
+
+
 }
